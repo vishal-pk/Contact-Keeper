dc330e21a7831ff98bb482040be73b04465eb324
vishal <pk.vishal24@gmail.com>
2020-02-28 19:12:55 +0530

    changed final

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db=config.get("mongoURI");
 const connectDB=()=>{
 	mongoose.connect(db,{
 		useNewUrlParser:true,
-		useCreateInsex:true,
+		useCreateIndex:true,
 		useFindAndModify:false,
 		useUnifiedTopology:true
 	})
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api/contacts",require("./routes/contact"));
 
 if(process.env.NODE_ENV==="production")
 {
-  app.use(exprss.static("client/build"));
+  app.use(express.static("client/build"));
   app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,"client","build","index.html")));
 }
 const port=process.env.PORT || 2000;
